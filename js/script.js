c6cdc67793b5243a811f12d18f3ef1a190582d90
Zhijiang Yang <yzjkid9@gmail.com>
2021-01-17 00:42:15 +0800

    :construction: added movement demo

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 /*
- * @Description: js for snake
+ * @Description:
  * @Author: Kotori Y
  * @Date: 2021-01-15 21:42:18
  * @LastEditors: Kotori Y
- * @LastEditTime: 2021-01-16 20:28:40
+ * @LastEditTime: 2021-01-17 00:35:07
  * @FilePath: \kotori-snake\js\script.js
  * @AuthorMail: kotori@cbdd.me
  */
@@ -16,6 +16,7 @@ async function interval(callback, delay) {
   });
 }
 
+// async sleeper(delay)
 
 class Snake {
   constructor(elem, initX, initY, length) {
@@ -25,11 +26,12 @@ class Snake {
     this.initY = initY;
     this.length = length;
     this.width = 20;
-    this.speed = 10; // 1s move one grid --> 20px
-    this.status = "alive"
-    this.head = null
-    this.tail = null
-    this.snake = null
+    this.height = 20;
+    this.speed = 50; // 1s move one grid --> 20px
+    this.status = "alive";
+    this.head = null;
+    this.tail = null;
+    this.snake = null;
     /* direction
         2
       4   6
@@ -42,18 +44,19 @@ class Snake {
     for (let idx = 1; idx <= this.length; idx++) {
       this.addBody(this.initX - this.width * idx, this.initY);
     }
-    this.snake = document.querySelectorAll('.snake-body')
-    this.tail = this.snake[this.length]
+    this.snake = document.querySelectorAll(".snake-body");
+    this.tail = this.snake[this.length];
 
-    this.move();
+    this.turn();
+    // this.move();
   }
 
   createHeader() {
     var head = document.createElement("div");
-    head.classList = "snake-body header"
+    head.classList = "snake-body header";
     head.style.left = `${this.initX}px`;
     head.style.top = `${this.initY}px`;
-    this.head = head
+    this.head = head;
     this.elem.appendChild(head);
   }
 
@@ -65,27 +68,70 @@ class Snake {
     this.elem.appendChild(body);
   }
 
-  move() {
-    interval((resolve, id)=>{
-      
-
-      this.tail.style.left = `${this.head.offsetLeft+this.width}px`
-      this.head.classList.remove("header")
-      this.tail.classList.add("header")
-
-      this.head = this.tail
-      this.tail = this.tail.previousElementSibling || this.snake[this.length]
-      
-      console.log(this.head.offsetLeft)
-
-      if (this.head.offsetLeft >= 1580) {
-        clearInterval(id)
+  turn() {
+    document.querySelector(".game-area").addEventListener("keydown", (e) => {
+      switch (e.code) {
+        case "ArrowUp":
+          this.direction = 2;
+          break;
+        case "ArrowDown":
+          this.direction = 8;
+          break;
+        case "ArrowLeft":
+          this.direction = 4;
+          break;
+        case "ArrowRight":
+          this.direction = 6;
+          break;
+        default:
+          this.direction = this.direction;
       }
-      
-    }, this.speed)
+    });
   }
 
+  move() {
+    interval((resolve, id) => {
+      switch (this.direction) {
+        case 2:
+          this.tail.style.left = `${this.head.offsetLeft}px`;
+          this.tail.style.top = `${this.head.offsetTop - this.height}px`;
+          break;
+        case 8:
+          this.tail.style.left = `${this.head.offsetLeft}px`;
+          this.tail.style.top = `${this.head.offsetTop + this.height}px`;
+          break;
+        case 4:
+          this.tail.style.top = `${this.head.offsetTop}px`;
+          this.tail.style.left = `${this.head.offsetLeft - this.width}px`;
+          break;
+        case 6:
+          this.tail.style.top = `${this.head.offsetTop}px`;
+          this.tail.style.left = `${this.head.offsetLeft + this.width}px`;
+          break;
+      }
+
+      this.head.classList.remove("header");
+      this.tail.classList.add("header");
+
+      this.head = this.tail;
+      this.tail = this.tail.previousElementSibling || this.snake[this.length];
+
+      console.log(this.head.offsetLeft);
+
+      if (this.head.offsetLeft >= 1580) {
+        clearInterval(id);
+      }
+    }, this.speed);
+  }
 }
 
-elem = document.querySelector('.snake')
-snake = new Snake(elem, 840, 800, 5)
+class Board {}
+
+elem = document.querySelector(".snake");
+snake = new Snake(elem, 600, 800, 30);
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    snake.move();
+  }
+});
