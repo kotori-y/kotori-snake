d22f107a1e108d0c6cb631759281b811747c8420
Zhijiang Yang <yzjkid9@gmail.com>
2021-01-19 17:05:25 +0800

    :construction: improved the movement operation

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 /*
- * @Description:
+ * @Description: script for my snake
  * @Author: Kotori Y
  * @Date: 2021-01-15 21:42:18
  * @LastEditors: Kotori Y
- * @LastEditTime: 2021-01-17 00:35:07
+ * @LastEditTime: 2021-01-19 17:02:35
  * @FilePath: \kotori-snake\js\script.js
  * @AuthorMail: kotori@cbdd.me
  */
@@ -19,19 +19,19 @@ async function interval(callback, delay) {
 // async sleeper(delay)
 
 class Snake {
+  #head = null;
+  #tail = null;
+  #snake = null;
+  #width = 20;
+  #height = 20;
   constructor(elem, initX, initY, length) {
     /* {height:20px; width:20px} */
     this.elem = elem;
     this.initX = initX;
     this.initY = initY;
     this.length = length;
-    this.width = 20;
-    this.height = 20;
     this.speed = 50; // 1s move one grid --> 20px
-    this.status = "alive";
-    this.head = null;
-    this.tail = null;
-    this.snake = null;
+    this.status = "sleep"; // ["sleep", "alive", "dead"]
     /* direction
         2
       4   6
@@ -40,23 +40,20 @@ class Snake {
     */
     this.direction = 6;
 
-    this.createHeader();
+    this.#createHeader();
     for (let idx = 1; idx <= this.length; idx++) {
-      this.addBody(this.initX - this.width * idx, this.initY);
+      this.addBody(this.initX - this.#width * idx, this.initY);
     }
-    this.snake = document.querySelectorAll(".snake-body");
-    this.tail = this.snake[this.length];
-
-    this.turn();
-    // this.move();
+    this.#snake = document.querySelectorAll(".snake-body");
+    this.#tail = this.#snake[this.length];
   }
 
-  createHeader() {
+  #createHeader() {
     var head = document.createElement("div");
     head.classList = "snake-body header";
     head.style.left = `${this.initX}px`;
     head.style.top = `${this.initY}px`;
-    this.head = head;
+    this.#head = head;
     this.elem.appendChild(head);
   }
 
@@ -68,70 +65,90 @@ class Snake {
     this.elem.appendChild(body);
   }
 
-  turn() {
-    document.querySelector(".game-area").addEventListener("keydown", (e) => {
-      switch (e.code) {
-        case "ArrowUp":
-          this.direction = 2;
-          break;
-        case "ArrowDown":
-          this.direction = 8;
-          break;
-        case "ArrowLeft":
-          this.direction = 4;
-          break;
-        case "ArrowRight":
-          this.direction = 6;
-          break;
-        default:
-          this.direction = this.direction;
-      }
-    });
-  }
-
-  move() {
+  #move() {
     interval((resolve, id) => {
       switch (this.direction) {
         case 2:
-          this.tail.style.left = `${this.head.offsetLeft}px`;
-          this.tail.style.top = `${this.head.offsetTop - this.height}px`;
+          this.#tail.style.left = `${this.#head.offsetLeft}px`;
+          this.#tail.style.top = `${this.#head.offsetTop - this.#height}px`;
           break;
         case 8:
-          this.tail.style.left = `${this.head.offsetLeft}px`;
-          this.tail.style.top = `${this.head.offsetTop + this.height}px`;
+          this.#tail.style.left = `${this.#head.offsetLeft}px`;
+          this.#tail.style.top = `${this.#head.offsetTop + this.#height}px`;
           break;
         case 4:
-          this.tail.style.top = `${this.head.offsetTop}px`;
-          this.tail.style.left = `${this.head.offsetLeft - this.width}px`;
+          this.#tail.style.top = `${this.#head.offsetTop}px`;
+          this.#tail.style.left = `${this.#head.offsetLeft - this.#width}px`;
           break;
         case 6:
-          this.tail.style.top = `${this.head.offsetTop}px`;
-          this.tail.style.left = `${this.head.offsetLeft + this.width}px`;
+          this.#tail.style.top = `${this.#head.offsetTop}px`;
+          this.#tail.style.left = `${this.#head.offsetLeft + this.#width}px`;
           break;
       }
 
-      this.head.classList.remove("header");
-      this.tail.classList.add("header");
+      this.#head.classList.remove("header");
+      this.#tail.classList.add("header");
 
-      this.head = this.tail;
-      this.tail = this.tail.previousElementSibling || this.snake[this.length];
+      this.#head = this.#tail;
+      this.#tail =
+        this.#tail.previousElementSibling || this.#snake[this.length];
 
-      console.log(this.head.offsetLeft);
+      console.log(this.#head.offsetLeft);
 
-      if (this.head.offsetLeft >= 1580) {
-        clearInterval(id);
-      }
+      this.#head.offsetLeft >= 0 &&
+      this.#head.offsetLeft <= 1580 &&
+      this.#head.offsetTop >= 0 &&
+      this.#head.offsetTop <= 880
+        ? resolve()
+        : clearInterval(id);
     }, this.speed);
+  }
+
+  move() {
+    this.#move();
+    this.status = "alive";
   }
 }
 
-class Board {}
+class Controller extends Snake {
+  constructor(elem, initX, initY, length) {
+    super(elem, initX, initY, length);
+
+    this.#startGame();
+  }
+
+  #startGame() {
+    var myFunc = (e) => {
+      if (e.key === "Enter") {
+        this.elem.parentNode.focus();
+        this.elem.parentNode.addEventListener("keydown", this.#turn);
+        this.move();
+        document.removeEventListener("keydown", myFunc);
+      }
+    };
+
+    document.addEventListener("keydown", myFunc);
+  }
+
+  #turn = (e) => {
+    switch (e.code) {
+      case "ArrowUp":
+        this.direction = this.direction != 8 ? 2 : 8;
+        break;
+      case "ArrowDown":
+        this.direction = this.direction != 8 ? 8 : 2;
+        break;
+      case "ArrowLeft":
+        this.direction = this.direction != 6 ? 4 : 6;
+        break;
+      case "ArrowRight":
+        this.direction = this.direction != 4 ? 6 : 4;
+        break;
+      default:
+        this.direction = this.direction;
+    }
+  };
+}
 
 elem = document.querySelector(".snake");
-snake = new Snake(elem, 600, 800, 30);
-
-document.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
-    snake.move();
-  }
-});
+game = new Controller(elem, 600, 800, 30);
