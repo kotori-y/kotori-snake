affaa6af88c0a9f497aebdfb7dedd787046f6cb4
Zhijiang Yang <yzjkid9@gmail.com>
2021-01-20 21:15:01 +0800

    :construction: added food class

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
  * @Author: Kotori Y
  * @Date: 2021-01-15 21:42:18
  * @LastEditors: Kotori Y
- * @LastEditTime: 2021-01-20 19:08:44
+ * @LastEditTime: 2021-01-20 21:14:16
  * @FilePath: \kotori-snake\js\script.js
  * @AuthorMail: kotori@cbdd.me
  */
@@ -16,7 +16,27 @@ async function interval(callback, delay) {
   });
 }
 
-class Snake {
+class Food {
+  constructor(foodElem) {
+    this.foodElem = foodElem;
+    this.areaWidth = snakeElem.parentElement.clientWidth;
+    this.areaHeight = snakeElem.parentElement.clientHeight;
+  }
+
+  createFood(width, height) {
+    this.foodElem.style.width = `${width}px`;
+    this.foodElem.style.height = `${height}px`;
+
+    this.foodElem.style.left = `${
+      Math.floor(Math.random() * ((this.areaWidth - width) / width)) * width
+    }px`;
+    this.foodElem.style.top = `${
+      Math.floor(Math.random() * ((this.areaHeight - height) / height)) * height
+    }px`;
+  }
+}
+
+class Snake extends Food {
   #head = null;
   #tail = null;
   #snake = null;
@@ -24,15 +44,15 @@ class Snake {
   #height = 20;
   #initX = null;
   #initY = null;
-  #areaHeight = null;
-  #areaWidth = null;
   allowTurn = false;
-  constructor(elem, length, speed) {
-    /* {height:20px; width:20px} */
-    this.elem = elem;
+  constructor(foodElem, snakeElem, length, speed) {
+    super(foodElem);
+    this.snakeElem = snakeElem;
     this.length = length;
     this.speed = speed; // 50ms move one grid --> 20px
     this.status = "sleep"; // ["sleep", "alive", "dead"]
+    this.score = 0;
+    this.eatNum = 0;
     /* direction
         2
       4   6
@@ -41,11 +61,8 @@ class Snake {
     */
     this.direction = 6;
 
-    this.#areaWidth = elem.parentElement.clientWidth;
-    this.#areaHeight = elem.parentElement.clientHeight;
-
-    this.#initX = this.#areaWidth / 2 + Math.ceil(length/2) * this.#width;
-    this.#initY = this.#areaHeight - 80;
+    this.#initX = this.areaWidth / 2 + Math.ceil(length / 2) * this.#width;
+    this.#initY = this.areaHeight - 80;
 
     this.#createHeader();
     for (let idx = 1; idx <= this.length; idx++) {
@@ -53,6 +70,7 @@ class Snake {
     }
     this.#snake = document.querySelectorAll(".snake-body");
     this.#tail = this.#snake[this.length];
+    this.#createFood(20, 20, false);
   }
 
   #createHeader() {
@@ -61,7 +79,21 @@ class Snake {
     head.style.left = `${this.#initX}px`;
     head.style.top = `${this.#initY}px`;
     this.#head = head;
-    this.elem.appendChild(head);
+    this.snakeElem.appendChild(head);
+  }
+
+  #createFood(width, height, withoutHead) {
+    super.createFood(width, height);
+    var bodys = withoutHead
+      ? document.querySelectorAll(".snake-body:not(.header)")
+      : document.querySelectorAll(".snake-body");
+
+    var overlap = Array.from(bodys).some((_body) =>
+      this.#isColliding(this.foodElem, _body)
+    );
+    if (overlap) {
+      this.#createFood(width, height, withoutHead);
+    }
   }
 
   addBody(xPos, yPos) {
@@ -69,7 +101,7 @@ class Snake {
     body.classList = "snake-body";
     body.style.left = `${xPos}px`;
     body.style.top = `${yPos}px`;
-    this.elem.appendChild(body);
+    this.snakeElem.appendChild(body);
   }
 
   #isColliding(div1, div2) {
@@ -77,7 +109,8 @@ class Snake {
     var d2Offset = { left: div2.offsetLeft, top: div2.offsetTop };
 
     var notColliding =
-      d1Offset.left !== d2Offset.left || d1Offset.top !== d2Offset.top;
+      Math.abs(d1Offset.left - d2Offset.left) >= 20 ||
+      Math.abs(d1Offset.top - d2Offset.top) >= 20;
 
     return !notColliding;
   }
@@ -93,35 +126,40 @@ class Snake {
   #isOverspill() {
     return !(
       this.#head.offsetLeft >= 0 &&
-      this.#head.offsetLeft <= this.#areaWidth - this.#width &&
+      this.#head.offsetLeft <= this.areaWidth - this.#width &&
       this.#head.offsetTop >= 0 &&
-      this.#head.offsetTop <= this.#areaHeight - this.#height
+      this.#head.offsetTop <= this.areaHeight - this.#height
     );
   }
 
+  #isAte() {
+    return this.#isColliding(this.#head, this.foodElem);
+  }
+
   #move() {
+    switch (this.direction) {
+      case 2:
+        this.#tail.style.left = `${this.#head.offsetLeft}px`;
+        this.#tail.style.top = `${this.#head.offsetTop - this.#height}px`;
+        break;
+      case 8:
+        this.#tail.style.left = `${this.#head.offsetLeft}px`;
+        this.#tail.style.top = `${this.#head.offsetTop + this.#height}px`;
+        break;
+      case 4:
+        this.#tail.style.top = `${this.#head.offsetTop}px`;
+        this.#tail.style.left = `${this.#head.offsetLeft - this.#width}px`;
+        break;
+      case 6:
+        this.#tail.style.top = `${this.#head.offsetTop}px`;
+        this.#tail.style.left = `${this.#head.offsetLeft + this.#width}px`;
+        break;
+    }
+  }
+
+  move() {
     interval((resolve, id) => {
-      switch (this.direction) {
-        case 2:
-          this.#tail.style.left = `${this.#head.offsetLeft}px`;
-          this.#tail.style.top = `${this.#head.offsetTop - this.#height}px`;
-          break;
-        case 8:
-          this.#tail.style.left = `${this.#head.offsetLeft}px`;
-          this.#tail.style.top = `${this.#head.offsetTop + this.#height}px`;
-          break;
-        case 4:
-          this.#tail.style.top = `${this.#head.offsetTop}px`;
-          this.#tail.style.left = `${this.#head.offsetLeft - this.#width}px`;
-          break;
-        case 6:
-          this.#tail.style.top = `${this.#head.offsetTop}px`;
-          this.#tail.style.left = `${this.#head.offsetLeft + this.#width}px`;
-          break;
-      }
-
-      this.allowTurn = true;
-
+      this.#move();
       this.#head.classList.remove("header");
       this.#tail.classList.add("header");
 
@@ -129,6 +167,16 @@ class Snake {
       this.#tail =
         this.#tail.previousElementSibling || this.#snake[this.length];
 
+      this.allowTurn = true;
+
+      var ate = this.#isAte();
+      if (ate) {
+        let [width, height, _score] =
+          Math.random() <= 0.1 ? [30, 30, 2] : [20, 20, 20];
+        this.eatNum++;
+        this.score += _score;
+        this.#createFood(width, height, true);
+      }
       var oversplill = this.#isOverspill();
       var bited = this.#isBited();
 
@@ -148,17 +196,11 @@ class Snake {
       }
     }, this.speed);
   }
-
-  move() {
-    this.#move();
-    this.status = "alive";
-  }
 }
 
-
 class Controller extends Snake {
-  constructor(elem, length, speed) {
-    super(elem, length, speed);
+  constructor(foodElem, snakeElem, length, speed) {
+    super(foodElem, snakeElem, length, speed);
 
     this.#startGame();
   }
@@ -166,8 +208,8 @@ class Controller extends Snake {
   #startGame() {
     var myFunc = (e) => {
       if (e.key === "Enter") {
-        this.elem.parentNode.focus();
-        this.elem.parentNode.addEventListener("keydown", this.#turn);
+        this.snakeElem.parentNode.focus();
+        this.snakeElem.parentNode.addEventListener("keydown", this.#turn);
         this.move();
         document.removeEventListener("keydown", myFunc);
       }
@@ -194,13 +236,11 @@ class Controller extends Snake {
         default:
           this.direction = this.direction;
       }
-      this.allowTurn = false
+      this.allowTurn = false;
     }
   };
 }
 
-
-
-
-elem = document.querySelector(".snake");
-game = new Controller(elem, 30, 100);
+snakeElem = document.querySelector(".snake");
+foodElem = document.querySelector(".food");
+game = new Controller(foodElem, snakeElem, 5, 100);
